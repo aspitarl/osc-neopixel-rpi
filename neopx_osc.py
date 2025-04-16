bc4ce3928fb9c6ae2e5b1fa17f35904b7e293786
Lee Aspitarte <aspitarte@gmail.com>
2025-04-15 21:39:52 -0700

    refactor into strip module

--- neopx_osc.py
+++ neopx_osc.py
@@ -4,39 +4,14 @@ This program listens to several addresses, and prints some information about
 received packets.
 """
 import argparse
-import math
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-import board
-import neopixel
-import colorsys
-
 import asyncio
 import socket
 
-class MyStrip():
-    def __init__(self, board_pin=board.D18, num_pixels=30):
-        self.h = 0
-        self.s = 1
-        self.v = 1
-        self.pixels = neopixel.NeoPixel(board_pin, num_pixels)
-
-    def set_hsv(self, address: str, val: float, *args):
-        if address == '/hue':
-            self.h = val
-        if address == '/sat':
-            self.s = val
-        if address == '/val':
-            self.v = val
-
-    def set_pixels(self):
-        r,g,b =  colorsys.hsv_to_rgb(self.h, self.s, self.v)
-        
-        for i in range(len(self.pixels)):
-            self.pixels[i] = (r*255,g*255,b*255)
-        self.pixels.show()
+from strip import MyStrip
 
 
 async def main_loop():
