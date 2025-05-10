7f5531d6734bef96951ee84d2c4fd29701be974d
Lee Aspitarte <aspitarte@gmail.com>
2025-05-09 23:47:06 -0700

    initial rainbombs and rainbow rain

--- presets.py
+++ presets.py
@@ -1,5 +1,6 @@
 import time
 import colorsys
+import random
 
 led_brightness = 0.2  # Default brightness for all presets
 
@@ -184,3 +185,129 @@ class Preset_Letters:
         pixels.show()
 
 preset_lookup['letters'] = Preset_Letters
+
+
+class Preset_RainbowRain(Preset_Rainbow):
+    def __init__(self, parent_strip):
+        super().__init__(parent_strip)
+        self.dot_spacing = 50  # Spacing between dots
+        self.dot_color = (0, 0, 0)  # Default color for the dots
+        self.dot_speed = 0.05  # Speed of the dots
+        self.dot_offset = 0  # Offset for the moving dots
+        self.time_updated_dots = time.time()
+
+        self.map_dispatcher()
+
+    def map_dispatcher(self):
+        super().map_dispatcher()
+        self.parent_strip.dispatcher.map('/dot_spacing', self.receive_message)
+        self.parent_strip.dispatcher.map('/dot_speed', self.receive_message)
+
+    def receive_message(self, address: str, val: float, *args):
+        super().receive_message(address, val, *args)
+        if address == '/dot_spacing':
+            self.dot_spacing = int(val)
+        if address == '/dot_speed':
+            self.dot_speed = val
+
+    def set_pixels(self):
+        pixels = self.parent_strip.pixels
+        num_pixels = len(pixels)
+
+        # Set the rainbow background
+        for i in range(num_pixels):
+            if i not in pixel_map_arr:
+                continue
+            i_hue = pixel_map_arr.index(i)
+            hue = ((i_hue + self.offset) % self.wavelength) / self.wavelength
+            r, g, b = colorsys.hsv_to_rgb(hue, 1, led_brightness)
+            pixels[i] = (int(r * 255), int(g * 255), int(b * 255))
+
+        # Update the offset for the rainbow animation
+        current_time = time.time()
+        if current_time - self.time_updated > self.wave_speed:
+            self.time_updated = current_time
+            self.offset += 1
+
+        # Add moving dots in the opposite direction
+        if current_time - self.time_updated_dots > self.dot_speed:
+            self.time_updated_dots = current_time
+            self.dot_offset = (self.dot_offset - 1) % self.dot_spacing
+
+        for i in range(0, num_pixels, self.dot_spacing):
+            dot_position = (i + self.dot_offset) % num_pixels
+            pixels[dot_position] = self.dot_color
+
+        pixels.write()
+        pixels.show()
+
+
+preset_lookup['rainbow_rain'] = Preset_RainbowRain
+
+class Preset_Rainbombs:
+    def __init__(self, parent_strip):
+        self.parent_strip = parent_strip
+        self.map_dispatcher()
+
+        self.bomb_interval = 0.1  # Time between bombs
+        self.bomb_speed = 0.05  # Speed of the expanding pixels
+        self.bomb_lifetime = 50  # Number of frames for each bomb
+        self.last_bomb_time = time.time()
+
+        self.active_bombs = []  # List of active bombs [(position, frame_count)]
+
+    def map_dispatcher(self):
+        self.parent_strip.dispatcher.map('/bomb_interval', self.receive_message)
+        self.parent_strip.dispatcher.map('/bomb_speed', self.receive_message)
+        self.parent_strip.dispatcher.map('/bomb_lifetime', self.receive_message)
+
+    def receive_message(self, address: str, val: float, *args):
+        if address == '/bomb_interval':
+            self.bomb_interval = val
+        elif address == '/bomb_speed':
+            self.bomb_speed = val
+        elif address == '/bomb_lifetime':
+            self.bomb_lifetime = int(val)
+
+    def set_pixels(self):
+        pixels = self.parent_strip.pixels
+        num_pixels = len(pixels)
+
+        # Check if it's time to spawn a new bomb
+        current_time = time.time()
+        if current_time - self.last_bomb_time >= self.bomb_interval:
+            self.last_bomb_time = current_time
+            bomb_position = random.randint(0, num_pixels - 1)
+            self.active_bombs.append((bomb_position, 0))  # Add new bomb with frame count 0
+
+        # Clear the strip
+        for i in range(num_pixels):
+            pixels[i] = (0, 0, 0)
+
+        # Update and draw active bombs
+        new_active_bombs = []
+        for bomb_position, frame_count in self.active_bombs:
+            if frame_count < self.bomb_lifetime:
+                # Calculate the positions of the expanding pixels
+                left_pixel = (bomb_position - frame_count) % num_pixels
+                right_pixel = (bomb_position + frame_count) % num_pixels
+
+                # Set the colors of the expanding pixels
+                r, g, b = colorsys.hsv_to_rgb(frame_count / self.bomb_lifetime, 1, led_brightness)
+                pixels[left_pixel] = (int(r * 255), int(g * 255), int(b * 255))
+                pixels[right_pixel] = (int(r * 255), int(g * 255), int(b * 255))
+
+                # Increment the frame count and keep the bomb active
+                new_active_bombs.append((bomb_position, frame_count + 1))
+
+        # Update the list of active bombs
+        self.active_bombs = new_active_bombs
+
+        # Write the updated pixel data
+        pixels.write()
+        pixels.show()
+
+
+preset_lookup['rainbombs'] = Preset_Rainbombs
+
+
