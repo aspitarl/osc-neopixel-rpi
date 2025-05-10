b5b7b8e5eeacc207a7d52c9717539ac62d9e0e80
Lee Aspitarte <aspitarte@gmail.com>
2025-05-09 20:44:06 -0700

    inital letters preset

--- presets.py
+++ presets.py
@@ -86,3 +86,69 @@ class Preset_Rainbow:
         # self.offset = (self.offset + 1) % self.wavelength
 
 preset_lookup['rainbow'] = Preset_Rainbow
+
+
+# this is a dictionary that maps letters to their corresponding pixel positions
+# the letters spell the word "elationstation"
+# The keys of the dict are unique: e1 l1, etc. 
+letter_lookup_dict = {
+    'e1': (0,24),
+    'l1': (30,48),
+    'a1': (60,81),
+    't1': (90,109),
+    'i1': (118,142),
+    'o1': (148,168),
+    'n1': (176,203),
+    's1': (205,225),
+    't2': (234,255),
+    'a2': (256,285),
+    't3': (286,315),
+    'i2': (315,337),
+    'o2': (350,369),
+    'n2': (378,410),
+}
+
+# Make a preset that sets each letter to a different color
+# one parameter should be the hue shift per letter
+
+class Preset_Letters:
+    def __init__(self, parent_strip):
+        self.parent_strip = parent_strip
+        self.map_dispatcher()
+
+        self.hue_shift = 2/14  # Default hue shift per letter
+        self.hue_shift_max = 1.0
+
+        self.idx_offset = 0  # Offset for animation
+        self.time_updated = time.time()  # Track the last update time
+
+        self.time_wait = 0.2  # Time to wait before updating the offset
+
+    def map_dispatcher(self):
+        self.parent_strip.dispatcher.map('/hue_shift', self.receive_message)
+
+    def receive_message(self, address: str, val: float, *args):
+        if address == '/hue_shift':
+            self.hue_shift = val * self.hue_shift_max
+
+    def set_pixels(self):
+        pixels = self.parent_strip.pixels
+        num_pixels = len(pixels)
+
+        # Increment idx_offset every second
+        current_time = time.time()
+        if current_time - self.time_updated >= self.time_wait:
+            self.time_updated = current_time
+            self.idx_offset -= 1
+
+        for letter, (start, end) in letter_lookup_dict.items():
+            letter_idx = list(letter_lookup_dict.keys()).index(letter) + self.idx_offset
+            hue = (letter_idx * self.hue_shift) % 1.0
+            r, g, b = colorsys.hsv_to_rgb(hue, 1, 0.10)
+            for i in range(start, end):
+                pixels[i] = (int(r * 255), int(g * 255), int(b * 255))
+
+        pixels.write()
+        pixels.show()
+
+preset_lookup['letters'] = Preset_Letters
